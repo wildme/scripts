8763810448a734598215c2c78eb5e82c95d5a1b5
Konstantin Ashaev <kostya@gorgaz.ru>
2025-03-03 10:06:57 +0300

    Do not save found lnk's path to the file

--- modify_win_shortcuts/modify_win_shortcuts.py
+++ modify_win_shortcuts/modify_win_shortcuts.py
@@ -4,21 +4,14 @@ import winshell
 
 def find_win_shortcuts(start_dir: str) -> list[str]:
     lnk_paths: list[str] = []
-    cwd: str = os.getcwd()
-    out_file = open(f'{cwd}\\lnk_files.txt', 'w')
-    tmp_full_path: str = ''
 
     for top, dirs, files in os.walk(start_dir):
         if not files:
             continue
-        # select only .lnk files
+        # select .lnk files
         for lnk in [_ for _ in files if _.endswith('.lnk')]:
-            tmp_full_path = os.path.join(top, lnk)
             # save the path in the list
-            lnk_paths.append(tmp_full_path)
-            # save the path to the file
-            print(tmp_full_path, file=out_file)
-    out_file.close()
+            lnk_paths.append(os.path.join(top, lnk))
 
     return lnk_paths
 
