123b577ab19f859fe3a69485501adcb0d4977745
Konstantin Ashaev <kostya@gorgaz.ru>
2025-03-19 12:48:45 +0300

    New function: update_counter(). Show file counter

--- modify_win_shortcuts/modify_win_shortcuts.py
+++ modify_win_shortcuts/modify_win_shortcuts.py
@@ -2,9 +2,16 @@ import os
 import sys
 import winshell
 
+def update_counter(cnt: int) -> None:
+    sys.stdout.write('\b' * len(str(eval('cnt - 1'))))
+    sys.stdout.write(str(cnt))
+    sys.stdout.flush()
+
 def find_win_shortcuts(start_dir: str) -> list[str]:
     lnk_paths: list[str] = []
+    counter: int = 0
 
+    print("Found files: %s" % (counter,), end='')
     for top, dirs, files in os.walk(start_dir):
         if not files:
             continue
@@ -12,6 +19,9 @@ def find_win_shortcuts(start_dir: str) -> list[str]:
         for lnk in [_ for _ in files if _.endswith('.lnk')]:
             # save the path in the list
             lnk_paths.append(os.path.join(top, lnk))
+            counter += 1
+            update_counter(counter)
+    print()
 
     return lnk_paths
 
