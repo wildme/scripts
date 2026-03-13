7d5d33450d5e52e67bb7fe8534192ccba2dedf64
Konstantin Ashaev <kostya@gorgaz.ru>
2026-03-13 16:05:50 +0300

    New file: select_subnets

---
+++ select_subnets/select_subnets.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import sys
+import json
+
+def print_subnets(subnets: set[str]) -> None:
+    for _ in subnets:
+        print(_, end=',') # print the resulting set
+    sys.stdout.write('\b \b\n') # delete the last comma and insert a new line
+
+def subnets_for(web_target: dict[str, str]) -> set[str]:
+    subs: set[str] = set()
+
+    for k in web_target.keys():
+        if isinstance(web_target[k], list): # this is a list of subnets
+            for _ in web_target[k]:
+                subs.add(_)
+        else:
+            subs.add(web_target[k]) # this is a string
+    return subs
+
+def main(web_targets: list[str]) -> None:
+    db_file: str = 'subnets.json'
+    data: dict[str, str] = {}
+    res: set[str] = set()
+    tmp: set[str] = set()
+
+    with open(db_file, 'r') as file:
+        data = json.load(file)
+
+    for _ in web_targets:
+        try:
+            tmp = subnets_for(data[_])
+        except KeyError:
+            print(f'{_} target not found. Check {db_file} for available targets')
+        res.update(tmp)
+
+    if len(res) > 0:
+        print_subnets(res)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        print('Please, specify at least one argument.\nExiting...')
+        sys.exit(1)
+    main(sys.argv[1:])
